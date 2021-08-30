eca05c9279838a96cba9a27fb165f7f4789334fe
matthew-samyn <84380381+matthew-samyn@users.noreply.github.com>
2021-08-30 11:17:04 +0200

    Update 30-08

--- json_to_df_v2.py
+++ json_to_df_v2.py
@@ -6,11 +6,13 @@ from collections import OrderedDict
 pd.set_option("display.max_columns",None)
 pd.set_option("display.max_rows",50)
 
+# Opening JSON
 with open("data_files/database.json", "r") as json_file:
     steam_games = json.load(json_file)
 
 print(f"We have this many id's: {len(steam_games.keys())}" )
 
+# Getting easy-to-extract-features in dataframe
 def get_features(key, games):
     ids = list(games.keys())
     column = []
@@ -23,7 +25,6 @@ def get_features(key, games):
     print(f"Column '{key}': {len(column)}")
     return column
 
-
 easy_lst = ["name", "required_age", "is_free", "developers", "review_score",
             "total_positive", "total_negative", "total_reviews"]
 
@@ -35,8 +36,8 @@ df_easy = pd.DataFrame(features_dct)
 df_easy["developers"] = df_easy["developers"].fillna(value="Unknown")
 df_easy['developers'] = df_easy.developers.apply(lambda x: x[0])
 df_easy['developers'] = df_easy.developers.replace("U","Unknown")
-print(df_easy[df_easy["developers"] == "Unknown"])
 
+# Getting genres and genre_id in dataframe
 genres_dct = get_features("genres", steam_games)
 genre_dct = OrderedDict()
 for lst in genres_dct:
@@ -52,6 +53,7 @@ df_genre["genre"] = list(genre_dct.values())
 df_genre.sort_values("id", inplace=True)
 df_genre.reset_index(inplace=True, drop=True)
 
+# Getting all the genre_id for each game_id in dataframe
 game_ids = []
 genre_ids = []
 
@@ -62,10 +64,9 @@ for game_id in steam_games:
                 genre_ids.append(genres_dct["id"])
     except:
         continue
-
 df_games_genre = pd.DataFrame({"game_id":game_ids, "genre_id":genre_ids})
 
-
+# Writing dataframes to csv
 df_games_genre.to_csv("data_files/gamesid_genreid.csv")
 df_genre.to_csv("data_files/different_genres.csv")
 df_easy.to_csv("data_files/steam_games_v2.csv")
