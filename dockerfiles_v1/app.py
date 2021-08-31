51c06b0915d8214b2863b6dfe3d179eaec181c68
Maarten Van den Bulcke <maarten.vdbul@gmail.com>
2021-08-31 11:05:24 +0200

    clean directory

---
+++ dockerfiles_v1/app.py
@@ -0,0 +1,43 @@
+import sqlite3
+import streamlit as st
+import plotly.express as px
+
+conn = sqlite3.connect('database/steam_data_v2.db')
+curs = conn.cursor()
+
+curs.execute('''
+SELECT COUNT(publishers)
+FROM Games
+GROUP BY publishers
+HAVING COUNT(publishers)>10
+ORDER BY COUNT(publishers);''')
+publishers_count = curs.fetchall()
+
+curs.execute(''' 
+SELECT publishers
+FROM Games
+GROUP BY publishers
+HAVING COUNT(publishers)>10
+ORDER BY COUNT(publishers);''')
+publishers = curs.fetchall()
+
+lst=[]
+for publisher in publishers:
+     lst.append(publisher[0])
+
+lst_count=[]
+for publisher in publishers_count:
+     lst_count.append(publisher[0])
+
+
+# Streamlit
+st.title("Top steam publishers")
+st.markdown("The chart is very nice")
+
+# Plotly
+fig = px.bar(y=lst, x=lst_count, orientation='h', labels={'x':'Game count', 'y':'Publishers'},
+             color_discrete_sequence=['indianred'], height=500)
+fig.update_layout(title_text='Top publishers by number of games on Steam', title_x=0.6)
+st.plotly_chart(fig, use_container_width=True)
+
+conn.close()
---
+++ dockerfiles_v1/utils/json_to_df_v1.py
@@ -0,0 +1,30 @@
+import json
+import pandas as pd
+import sqlite3
+
+data = open("../data_files/database.json")
+
+steam_games = json.load(data)
+
+features = ["name", "publishers"]
+def get_values_for_one_game(data: json, values: list) -> pd.DataFrame:
+    ids = data.keys()
+
+    dct = {"id":ids}
+
+    for value in values:
+        lst = []
+        for id in ids:
+            lst.append(steam_games[id][value])
+        dct[value] = lst
+
+    return pd.DataFrame(dct)
+
+df = get_values_for_one_game(steam_games, features)
+print(df.shape)
+print(df.isna().any())
+df = df.drop_duplicates("id")
+print(df.shape)
+df['publishers'] = df.publishers.apply(lambda x: x[0])
+
+df.to_csv("../data_files/steam_games_v1.csv", index=False)
