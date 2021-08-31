e9f70c6ea8a0a3990aa7d3132010d277be4e1b7a
lvendrix <hello@loganvendrix.com>
2021-09-01 00:14:46 +0200

    random button, steam link, etc.

--- dockerfiles_v3/app_v3.py
+++ dockerfiles_v3/app_v3.py
@@ -27,68 +27,96 @@ for game_name in list_games_query:
 
 # Streamlit
 image = Image.open('steam_logo.png')
-#st.title("Steam Dashboard 🎮")
 
-box_1 = st.sidebar.selectbox('What do you want to do?',
-                                    ('Select', 'Analyse one game', 'Compare two games', 'Compare all games'))
+st.sidebar.title("Steam Dashboard")
+box_1 = st.sidebar.selectbox('What do you want to analyze?', ('Select', 'Single game', 'Two games', 'All games'))
 
 if box_1 == 'Select':
     st.image(image)
 
-elif box_1 == 'Analyse one game':
-    box_1_1 = st.sidebar.selectbox('Select the game',
-                                   list_games)
+elif box_1 == 'Single game':
+    # if users want to use random
+    if st.sidebar.button('Random'):
+        box_1_1 = st.sidebar.selectbox('Select game', [random.choice(list_games)])
+        new_list = list(list_games)
+        # if user wants to reset
+        if st.sidebar.button('Reset'):
+            box_1_1 = st.sidebar.selectbox('Select game', list_games)
+            new_list = list(list_games)
 
-    box_1_2 = st.sidebar.selectbox('Select desired feature',
-                                   (['Reviews']))
-    if box_1_2 == 'Reviews':
+    # if user selects on his own
+    else:
+        box_1_1 = st.sidebar.selectbox('Select game', list_games)
+        new_list = list(list_games)
+
+    box_1_2 = st.sidebar.selectbox('Select desired feature', (['Select', 'Reviews']))
+
+    # Description
+    descr = conn.cursor()
+    query = f'SELECT short_description FROM Games WHERE Games.name = ?'
+    params = [box_1_1]
+    descr.execute(query, params)
+
+    # Image
+    img = conn.cursor()
+    query = f'SELECT header_image FROM Games WHERE Games.name = ?'
+    params = [box_1_1]
+    img.execute(query, params)
+
+    # id
+    id_query = conn.cursor()
+    query = f'SELECT id FROM Games WHERE Games.name = ?'
+    params = [box_1_1]
+    id_query.execute(query, params)
+
+    if box_1_2 == 'Select':
+        link_steam = f"https://store.steampowered.com/app/{id_query.fetchone()[0]}/"
+        col1, col2, col3 = st.columns([1, 6, 1])
+        with col1:
+            st.write("")
+        with col2:
+            st.markdown(f"[![image_game]({img.fetchone()[0]})]({link_steam})")
+        with col3:
+            st.write("")
+
+        # st.title(f'{box_1_1}')
+        # Description
+        st.text(descr.fetchone()[0])
+
+    elif box_1_2 == 'Reviews':
         # Name
         curs = conn.cursor()
         query = f'SELECT total_positive, total_negative FROM Games WHERE Games.name = ?'
         params = [box_1_1]
         curs.execute(query, params)
 
-        # Description
-        descr = conn.cursor()
-        query = f'SELECT short_description FROM Games WHERE Games.name = ?'
-        params = [box_1_1]
-        descr.execute(query, params)
-
-        # Image
-        img = conn.cursor()
-        query = f'SELECT header_image FROM Games WHERE Games.name = ?'
-        params = [box_1_1]
-        img.execute(query, params)
-
-        col1, col2, col3 = st.columns([1, 6, 1])
-        with col1:
-            st.write("")
-        with col2:
-            st.markdown(f"![Foo]({img.fetchone()[0]})")
-        with col3:
-            st.write("")
-
-        #st.title(f'{box_1_1}')
-        st.text(descr.fetchone()[0])
-
+        st.markdown(f"<h1 style='text-align: center;'>{box_1_1}</h1>", unsafe_allow_html=True)
         fig = px.pie(values=curs.fetchone(), names=['Positive reviews', 'Negative reviews'])
         fig.update_layout(title_text=f'Reviews for {box_1_1}', title_x=0.5)
         fig.update_traces(textposition='inside', textinfo='value+percent')
         st.plotly_chart(fig, use_container_width=True)
 
-elif box_1 == 'Compare two games':
-    #st.sidebar.text('Select desired games')
-    #if st.sidebar.button('Random'):
-    #    random.choice(list_games)
+elif box_1 == 'Two games':
+    # if random button clicked
+    if st.sidebar.button('Random'):
+        box_1_1 = st.sidebar.selectbox('Select game #1', [random.choice(list_games)])
+        new_list = list(list_games)
+        new_list.remove(box_1_1)
+        box_1_2 = st.sidebar.selectbox('Select game #2', [random.choice(new_list)])
+        # if user wants to reset
+        if st.sidebar.button('Reset'):
+            box_1_1 = st.sidebar.selectbox('Select game #1', list_games)
+            new_list = list(list_games)
+            new_list.remove(box_1_1)
+            box_1_2 = st.sidebar.selectbox('Select game #2', new_list)
 
+    # if user select two games
+    else:
+        box_1_1 = st.sidebar.selectbox('Select game #1', list_games)
+        new_list = list(list_games)
+        new_list.remove(box_1_1)
+        box_1_2 = st.sidebar.selectbox('Select game #2', new_list)
 
-    box_1_1 = st.sidebar.selectbox('Select game #1',
-                                   list_games)
-    new_list = list(list_games)
-    new_list.remove(box_1_1)
-
-    box_1_2 = st.sidebar.selectbox('Select game #2',
-                                   new_list)
     box_1_3 = st.sidebar.selectbox('Select features to compare',
                                    (['Select', 'Price', 'Reviews']))
 
@@ -98,18 +126,22 @@ elif box_1 == 'Compare two games':
         st.markdown(f"<h1 style='text-align: center;'>vs</h1>", unsafe_allow_html=True)
         st.markdown(f"<h1 style='text-align: center;'>{box_1_2}</h1>", unsafe_allow_html=True)
         st.markdown(f"<h1 style='text-align: center;'></h1>", unsafe_allow_html=True)
-        st.markdown("![Foo](https://i.gifer.com/origin/57/570998fe4bf6c8c6827a3cb1c3d23003_w200.gif)")
-        #st.markdown("![Fighter](https://tenor.com/9WU4.gif)")
-        #st.write(box_1_1)
+
+        # Gif here?
+        #col1, col2 = st.columns(2)
+        #col1.markdown("![Foo](https://i.gifer.com/origin/57/570998fe4bf6c8c6827a3cb1c3d23003_w200.gif)")
+        #col2.markdown("![Foo](https://i.gifer.com/4dg6.gif)")
+
     elif box_1_3 == 'Price':
-        # Price
+        # Price PROBLEM! Keeps changing?!
         price_query = conn.cursor()
         query = f'SELECT price FROM Games WHERE Games.name = ? or Games.name = ?'
         params = list_name_games
         price_query.execute(query, params)
 
+        st.markdown(f"<h1 style='text-align: center;'>Price: {box_1_1} vs {box_1_2}</h1>", unsafe_allow_html = True)
         fig = px.bar(x=list_name_games, y=[price_query.fetchone()[0], price_query.fetchone()[0]])
-        fig.update_layout(title_text=f'Price of {box_1_1} vs {box_1_2}', title_x=0.5)
+        #fig.update_layout(title_text=f'Price of {box_1_1} vs {box_1_2}', title_x=0.5)
         fig.update_xaxes(title_text='Games')
         fig.update_yaxes(title_text='Price')
         st.plotly_chart(fig, use_container_width=True)
@@ -121,13 +153,14 @@ elif box_1 == 'Compare two games':
         params = list_name_games
         review_query.execute(query, params)
 
+        st.markdown(f"<h1 style='text-align: center;'>Reviews: {box_1_1} vs {box_1_2}</h1>", unsafe_allow_html=True)
         fig = px.bar(x=list_name_games, y=[review_query.fetchone()[0], review_query.fetchone()[0]])
-        fig.update_layout(title_text=f'Number of positive reviews of {box_1_1} vs {box_1_2}', title_x=0.5)
+        #fig.update_layout(title_text=f'Number of positive reviews of {box_1_1} vs {box_1_2}', title_x=0.5)
         fig.update_xaxes(title_text='Games')
         fig.update_yaxes(title_text='Reviews')
         st.plotly_chart(fig, use_container_width=True)
 
-elif box_1 == 'Compare all games':
+elif box_1 == 'All games':
     box_1_1 = st.sidebar.selectbox('Select features',
                                    (['Select', 'Developers', 'Price', 'Reviews']))
     if box_1_1 == 'Developers':
